e4ffad482702883f5e0a716c37e574576723f1aa
cpkio <krasilnikov.cpkio@gmail.com>
2023-10-24 20:38:34 +0300

    Previous didnt work really

--- lua/fzf-ui-select/init.lua
+++ lua/fzf-ui-select/init.lua
@@ -11,20 +11,25 @@ M.ui_select = function(items, options, on_choice)
           vim.api.nvim_replace_termcodes(key, true, false, true), "n", true)
       end
 
-      local win_h = vim.api.nvim_win_get_height(0)
+      local win_info = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1]
+      local topline = win_info.topline
+      local botline = win_info.botline
+      local win_h = win_info.height - 2
       local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+      local pos = row - topline
 
-      local float_height = 0
+      local float_height = #items + 2
+      local float_height_max = 0
       local supplementary_fzf_options
-      local _row = 0
-      if row < math.ceil(win_h/2) then
+      local _row = 0 -- row to start drawing popup
+      if pos < math.ceil(win_h/2) then
         supplementary_fzf_options = (term.fzf_colors .. ' --padding=0,1,1,1 --ansi --reverse --prompt="Choice> "')
+        float_height_max = win_h - (pos + 1)
         _row = 1
-        float_height = win_h - row
       else
         supplementary_fzf_options = (term.fzf_colors .. ' --padding=1,1,0,1 --ansi --prompt="Choice> "')
-        _row = _row - #items - 2
-        float_height = row
+        float_height_max = pos - 1
+        _row = -math.min(float_height_max, float_height)
       end
 
       options = utils.normalize_opts({
@@ -35,7 +40,7 @@ M.ui_select = function(items, options, on_choice)
               row = _row,
               col = 0,
               width = 60,
-              height = math.min(#items + 2, float_height),
+              height = math.min(float_height_max, float_height),
               noautocmd = true
             })
         end
